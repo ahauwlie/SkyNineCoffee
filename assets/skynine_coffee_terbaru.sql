7d170faaaa8a45932589f1352a5a265e36010ff1
Hauw <ahauwlie@gmail.com>
2018-11-22 09:25:04 +0700

    84561
    2264864

--- assets/skynine_coffee_terbaru.sql
+++ assets/skynine_coffee_terbaru.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 14, 2018 at 01:52 PM
+-- Generation Time: Nov 22, 2018 at 02:24 AM
 -- Server version: 5.7.19
 -- PHP Version: 7.1.14
 
@@ -124,14 +124,14 @@ INSERT INTO `product` (`id_pr`, `nama_pr`, `tipe_pr`, `harga_pr`, `stock_pr`, `d
 (13, 'Kenya', 'Kopi', 90000, 568, 'Orange Mouthfeel, Clean After Taste & Brown Sugar\r\nProcess: Fullwash\r\nType: Arabica\r\nAcidity: 3\r\nBody: 4\r\nSweetness: 3', 'kenya.jpeg'),
 (14, 'Columbia', 'Kopi', 90000, 100, 'Chocolate, Honey like, Orange like. Medium Body, Medium Acidity, High Sweetness\r\nProcess: Fullwash\r\nType: Arabica\r\nAcidity: 3\r\nBody: 4\r\nSweetness: 3', 'colombia.jpeg'),
 (15, 'Bali Kintamani-50kg', 'Biji_Kopi', 6000000, 200, 'Natural\r\nType: Arabica', 'balikintamani.jpeg'),
-(16, 'Hario Paper Filter VCF-01-100W', 'Alat_Seduh', 85000, 200, 'Spesifikasi Produk:\r\n- Cocok untuk Hario/Tiamo V60 01\r\n- Kapasitas 1-2 cups\r\n- Isi per bungkus : 100 lembar\r\n- Made in Japan', 'hariopaper1.jpg'),
-(17, 'HARIO PAPER FILTER - VCF-01-100M', 'Alat_Seduh', 85000, 200, 'Spesifikasi Produk:\r\n- Cocok untuk Hario/Tiamo V60 01\r\n- Kapasitas 1-2 cups\r\n- Isi per bungkus : 100 lembar\r\n- Made in Japan', 'hariopaper.jpg'),
-(18, 'V-60 ELECTRIC COFFEE GRINDER - EVCG-8B', 'Alat_Seduh', 3300000, 200, 'Brand: Hario\r\nPower: AC 220V\r\nPower Consumption: 150W\r\nHopper Capacity: Maximum 8oz(240g)\r\nDimensi: 30x13.5x39cm\r\nCable Length: 1.3m\r\nConial: Stainless steel\r\nWeight: 2.4kg', 'v60.jpg'),
-(19, 'HARIO DRIPPER V-60 WHITE - VD-02W', 'Alat_Seduh', 100000, 200, 'Material: Japan Plastic\r\nBrand: Hario\r\nDimensi: 13.5x11.5x10cm', 'dripper.jpg'),
-(20, 'HARIO DRIPPER V-60 CERAMIC - VDC-02R', 'Alat_Seduh', 450000, 200, 'Material: Japan Plastic\r\nBrand: Hario\r\nSendok kopi plastik ukuran 12gr', 'dripper1.jpg'),
+(16, 'Paper Filter 100W', 'Alat_Seduh', 85000, 200, 'Spesifikasi Produk:\r\n- Cocok untuk Hario/Tiamo V60 01\r\n- Kapasitas 1-2 cups\r\n- Isi per bungkus : 100 lembar\r\n- Made in Japan', 'hariopaper1.jpg'),
+(17, 'Paper Filter 100M', 'Alat_Seduh', 85000, 200, 'Spesifikasi Produk:\r\n- Cocok untuk Hario/Tiamo V60 01\r\n- Kapasitas 1-2 cups\r\n- Isi per bungkus : 100 lembar\r\n- Made in Japan', 'hariopaper.jpg'),
+(18, 'V-60 coffee grinder', 'Alat_Seduh', 3300000, 200, 'Brand: Hario\r\nPower: AC 220V\r\nPower Consumption: 150W\r\nHopper Capacity: Maximum 8oz(240g)\r\nDimensi: 30x13.5x39cm\r\nCable Length: 1.3m\r\nConial: Stainless steel\r\nWeight: 2.4kg', 'v60.jpg'),
+(19, 'vd-02w dripper', 'Alat_Seduh', 100000, 200, 'Material: Japan Plastic\r\nBrand: Hario\r\nDimensi: 13.5x11.5x10cm', 'dripper.jpg'),
+(20, 'dripper v-60 keramik', 'Alat_Seduh', 450000, 200, 'Material: Japan Plastic\r\nBrand: Hario\r\nSendok kopi plastik ukuran 12gr', 'dripper1.jpg'),
 (21, 'Aeropress', 'Alat_Seduh', 590000, 200, 'Material BPA free, food grade\r\nMade in USA', 'aeropress.jpg'),
-(22, 'ABLE DISK COFFEE FILTER - FOR AEROPRESS', 'Alat_Seduh', 220000, 200, 'Brand: Able\r\nType: Standard\r\nMaterial: Stainless Stell\r\nDiameter: 6.3cm\r\nMade In USA', 'disk.jpg'),
-(23, 'HARIO DRIP SCALE - VST-2000B', 'Alat_Seduh', 1200000, 200, 'Brand: Hario\r\nType: Coffee Drip Scale\r\nMaterial: ABS Resin\r\nDimensi: 19x12x2.85\r\nBattery: 2pcs AAA\r\nAuto-off after 5 minutes', 'scale.jpg');
+(22, 'abledisk coffee filter', 'Alat_Seduh', 220000, 200, 'Brand: Able\r\nType: Standard\r\nMaterial: Stainless Stell\r\nDiameter: 6.3cm\r\nMade In USA', 'disk.jpg'),
+(23, 'drip scale vst-2000b', 'Alat_Seduh', 1200000, 200, 'Brand: Hario\r\nType: Coffee Drip Scale\r\nMaterial: ABS Resin\r\nDimensi: 19x12x2.85\r\nBattery: 2pcs AAA\r\nAuto-off after 5 minutes', 'scale.jpg');
 
 -- --------------------------------------------------------
 
